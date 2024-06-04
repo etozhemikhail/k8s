5d4b7d10255d0d4638627607ecb826d96c3a8cd3
Mikhail <etozhemikhail@icloud.com>
2024-06-04 12:06:33 +0000

    added monitoring

--- get_k8s_config.sh
+++ get_k8s_config.sh
@@ -12,3 +12,12 @@ scp -i ~/.ssh/id_rsa etozhemikhail@$IPK8S:/home/etozhemikhail/config "/home/etoz
 sed -i "s/lb-apiserver.kubernetes.local/${IPK8S}/g" "/home/etozhemikhail/.kube/config.${WS}"
 cp "/home/etozhemikhail/.kube/config.${WS}" "/home/etozhemikhail/.kube/config"
 export KUBECONFIG=$KUBECONFIG:$HOME/.kube/config.$WS
+
+
+echo -e "\n##################################################################"
+echo -e "# Устанавливаем kube-prometheus"
+echo -e "####################################################################\n"
+kubectl apply --server-side -f ../kube-prometheus/manifests/setup
+until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
+kubectl apply -f ../kube-prometheus/manifests/
+kubectl apply -f ../kube-prometheus/grafana-svc.yaml -f ../kube-prometheus/grafana-NP.yaml
